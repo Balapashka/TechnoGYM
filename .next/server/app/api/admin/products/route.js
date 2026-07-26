d395b1f574be58e016737e11730bff9578253c4d
Yeldos <babyknight@Yeldoss-MacBook-Pro.local>
2026-07-26 14:04:42 +0500

    changes

--- .next/server/app/api/admin/products/route.js
+++ .next/server/app/api/admin/products/route.js
@@ -1,5 +1,5 @@
 var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/admin/products/route.js")
-R.c("server/chunks/[root-of-the-server]__044qx00._.js")
+R.c("server/chunks/[root-of-the-server]__1c59zud._.js")
 R.c("server/chunks/_0htkyro._.js")
 R.c("server/chunks/[root-of-the-server]__0xuaoik._.js")
 R.c("server/chunks/node_modules_zod_v4_classic_external_1-pw2v2.js")
